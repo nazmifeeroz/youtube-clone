29cda939095a7affc2de3d2234778304e81d1b8f
nazmifeeroz <nazmifeeroz@hotmail.com>
2018-06-12 05:42:13 +0800

    setup youtube api search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import YTSearch from "youtube-api-search";
 import "./App.css";
 import SearchBar from "./components/search_bar";
 
+const API_KEY = "AIzaSyA4cYpjTeh7KY5aB419kMNkPjrge3ZmSus";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos =>
+      this.setState({ videos })
+    );
+  }
+
   render() {
     return (
       <div className="App">
